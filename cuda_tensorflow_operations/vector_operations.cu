d00eab11e9827ec73e0c8dd0c23485004573a08a
msaeedha <mo.saeed@nu.edu.eg>
2021-03-16 03:43:08 +0200

    implement the core functionalities of the kerenel and tested with samples

--- cuda_tensorflow_operations/vector_operations.cu
+++ cuda_tensorflow_operations/vector_operations.cu
@@ -7,16 +7,15 @@
 #include<cstdlib>
 #include<cmath>
 
-#define DEBUG
+//#define DEBUG
 
-__global__
-void kernel(int* vec, int* mat, int* out, const int N, const int M) {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    int sum = 0;
-    if (tid < M) {
-        for (int i = 0; i < N; i++)
-            sum += vec[i] * mat[(i * M) + tid];
-        out[tid] = sum;
+__global__ void kernel(int* a, int* b, int* c, int* d, const int N, const int M) {
+    int ThreadIndex = threadIdx.x + blockIdx.x * blockDim.x;
+#if defined(DEBUG)
+    printf("ThreadIndex: %d\n", ThreadIndex);
+#endif
+    if (ThreadIndex < (N * M)) {
+        d[ThreadIndex] = a[ThreadIndex % N] * b[ThreadIndex % M] + c[ThreadIndex];
     }
 }
 
@@ -25,47 +24,65 @@ void init_array(int* a, const int N);
 void init_mat(int* a, const int N, const int M);
 void print_array(int* a, const int N, char* d);
 void print_mat(int* a, const int N, const int M, char* d);
+int calculate_error(int* a, int* b, const int N, const int M);
+void cpu_calculate_array(int* a, int* b, int* c, int* d, const int N, const int M);
+
+
+void cpu_calculate_array(int* a, int* b, int* c, int* d, const int N, const int M) {
+    for (int iterationIndex = 0; iterationIndex < N * M; iterationIndex++)
+    {
+        d[iterationIndex] = a[iterationIndex % N] * b[iterationIndex % M] + c[iterationIndex];
+#if defined(DEBUG)
+        printf("[ Indices id:%d, aId:%d, bId:%d ] Inputs a:%d, b:%d, c:%d >> Output d:%d\n", iterationIndex, iterationIndex % N, iterationIndex % M, a[iterationIndex % N], b[iterationIndex % M], c[iterationIndex], d[iterationIndex]);
+#endif
+    }
+}
 
 int main(void) {
     srand(time(NULL));
 
-    int* a, * b, * c;
-    int* dev_a, * dev_b, * dev_c;
+    int* a, * b, * c, * d;
+    int* dev_a, * dev_b, * dev_c, * dev_d;
 
     int N = 3;
     int M = 4;
     a = (int*)malloc(sizeof(int) * N);
-    b = (int*)malloc(sizeof(int) * N * M);
-    c = (int*)malloc(sizeof(int) * M);
+    b = (int*)malloc(sizeof(int) * M);
+    c = (int*)malloc(sizeof(int) * N * M);
+    d = (int*)malloc(sizeof(int) * N * M);
+  
     init_array(a, N);
-    init_mat(b, N, M);
-    init_array(c, M);
+    init_array(b, M);
+    init_mat(c, N, M);
 
     printf("<<<<<<<<<< initial data:\n");
-    print_array(a, N, "in-vector");
-    print_mat(b, N, M, "matrix");
-    print_array(c, M, "out-vector");
+    print_array(a, N, "a-vector");
+    print_array(b, M, "b-vector");
+    print_mat(c, N, M, "c-matrix");
 
     cudaMalloc((void**)&dev_a, sizeof(int) * N);
-    cudaMalloc((void**)&dev_b, sizeof(int) * N * M);
-    cudaMalloc((void**)&dev_c, sizeof(int) * M);
+    cudaMalloc((void**)&dev_b, sizeof(int) * M);
+    cudaMalloc((void**)&dev_c, sizeof(int) * N * M);
+    cudaMalloc((void**)&dev_d, sizeof(int) * N * M);
 
     cudaMemcpy(dev_a, a, sizeof(int) * N, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, b, sizeof(int) * N * M, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, sizeof(int) * M, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_c, c, sizeof(int) * N * M, cudaMemcpyHostToDevice);
 
     printf("\n\nRunning Kernel...\n\n");
-    kernel << <M / 256 + 1, 256 >> > (dev_a, dev_b, dev_c, N, M);
+    kernel << <N * M / 256 + 1, 256 >> > (dev_a, dev_b, dev_c, dev_d, N, M);
 
     printf("Gpu Return with Error Code: %s\n",cudaGetErrorString(cudaGetLastError()));
 
-    cudaMemcpy(c, dev_c, sizeof(int) * M, cudaMemcpyDeviceToHost);
+    cudaMemcpy(d, dev_d, sizeof(int) * N * M, cudaMemcpyDeviceToHost);
 
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
+    cudaFree(dev_d);
 
     printf(">>>>>>>>>> final data:\n");
-    print_array(c, M, "out-vector");
+    print_mat(d, N, M, "d-matrix");
 
     return 0;
 };
@@ -73,13 +90,16 @@ int main(void) {
 void init_array(int* a, const int N) {
     int i;
     for (i = 0; i < N; i++)
+    {
         a[i] = rand() % 4 + 1;
+    }
 }
 void init_mat(int* a, const int N, const int M) {
     int i, j;
-    for (i = 0; i < N; i++)
-        for (j = 0; j < M; j++)
-            a[i * M + j] = rand() % 4 + 1;
+    for (i = 0; i < N * M; i++)
+    {
+        a[i] = rand() % 4 + 1;
+    }
 }
 void print_array(int* a, const int N, char* d) {
     int i;
@@ -88,11 +108,29 @@ void print_array(int* a, const int N, char* d) {
     printf("\n");
 }
 void print_mat(int* a, const int N, const int M, char* d) {
-    int i, j;
-    for (i = 0; i < N; i++) {
-        printf("\n%s[%d]:", d, i);
-        for (j = 0; j < M; j++)
-            printf("\t%d", a[i * M + j]);
+    int i;
+    for (i = 0; i < N*M; i++) {
+        if (i % M == 0)
+        {
+            printf("\n%s[%d]:", d, i);
+        }
+        printf("\t%d", a[i]);
     }
     printf("\n");
 }
+
+
+int calculate_error(int* a, int* b, const int N, const int M)
+{
+    int i, error_result = 0;
+    for (i = 0; i < N * M; i++)
+    {
+        if (abs(a[i] - b[i]) > 0)
+        {
+            printf("\tIndex %d has error InputA=%d , InputB=%d\n", i, a[i], b[i]);
+        }
+        error_result += abs(a[i] - b[i]);
+    }
+    return error_result;
+}
+
