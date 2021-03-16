59d463e7c47fa7218909d4410027cbc276c092f0
msaeedha <mo.saeed@nu.edu.eg>
2021-03-16 04:21:38 +0200

    refactor the naming rules of the variables

--- cuda_tensorflow_operations/vector_operations.cu
+++ cuda_tensorflow_operations/vector_operations.cu
@@ -9,31 +9,31 @@
 
 //#define DEBUG
 
-__global__ void kernel(int* a, int* b, int* c, int* d, const int N, const int M) {
+__global__ void kernel(int* a_InputArray, int* b_InputArray, int* AddMatrix, int* ResultMatrix, const int MatrixRows, const int MatrixColums) {
     int ThreadIndex = threadIdx.x + blockIdx.x * blockDim.x;
 #if defined(DEBUG)
     printf("ThreadIndex: %d\n", ThreadIndex);
 #endif
-    if (ThreadIndex < (N * M)) {
-        d[ThreadIndex] = a[ThreadIndex % N] * b[ThreadIndex % M] + c[ThreadIndex];
+    if (ThreadIndex < (MatrixRows * MatrixColums)) {
+        ResultMatrix[ThreadIndex] = a_InputArray[ThreadIndex % MatrixRows] * b_InputArray[ThreadIndex % MatrixColums] + AddMatrix[ThreadIndex];
     }
 }
 
 // debuging functions
-void init_array(int* a, const int N);
-void init_mat(int* a, const int N, const int M);
-void print_array(int* a, const int N, char* d);
-void print_mat(int* a, const int N, const int M, char* d);
-int calculate_error(int* a, int* b, const int N, const int M);
-void cpu_calculate_array(int* a, int* b, int* c, int* d, const int N, const int M);
+void init_array(int* pArray, const int MatrixRows);
+void init_mat(int* pMatrix, const int MatrixRows, const int MatrixColums);
+void print_array(int* pArray, const int MatrixRows, char* MatrixName);
+void print_mat(int* pMatrix, const int MatrixRows, const int MatrixColums, char* MatrixName);
+int calculate_error(int* p_a_Matrix, int* p_b_Matrix, const int MatrixRows, const int MatrixColums);
+void cpu_calculate_array(int* a_InputArray, int* b_InputArray, int* AddMatrix, int* ResultMatrix, const int MatrixRows, const int MatrixColums);
 
 
-void cpu_calculate_array(int* a, int* b, int* c, int* d, const int N, const int M) {
-    for (int iterationIndex = 0; iterationIndex < N * M; iterationIndex++)
+void cpu_calculate_array(int* a_InputArray, int* b_InputArray, int* AddMatrix, int* ResultMatrix, const int MatrixRows, const int MatrixColums) {
+    for (int LoopIndex = 0; LoopIndex < MatrixRows * MatrixColums; LoopIndex++)
     {
-        d[iterationIndex] = a[iterationIndex % N] * b[iterationIndex % M] + c[iterationIndex];
+        ResultMatrix[LoopIndex] = a_InputArray[LoopIndex % MatrixRows] * b_InputArray[LoopIndex % MatrixColums] + AddMatrix[LoopIndex];
 #if defined(DEBUG)
-        printf("[ Indices id:%d, aId:%d, bId:%d ] Inputs a:%d, b:%d, c:%d >> Output d:%d\n", iterationIndex, iterationIndex % N, iterationIndex % M, a[iterationIndex % N], b[iterationIndex % M], c[iterationIndex], d[iterationIndex]);
+        printf("[ Indices id:%d, aId:%d, bId:%d ] Inputs a:%d, b:%d, c:%d >> Output d:%d\n", LoopIndex, LoopIndex % MatrixRows, LoopIndex % MatrixColums, a_InputArray[LoopIndex % MatrixRows], b_InputArray[LoopIndex % MatrixColums], AddMatrix[LoopIndex], ResultMatrix[LoopIndex]);
 #endif
     }
 }
@@ -41,40 +41,40 @@ void cpu_calculate_array(int* a, int* b, int* c, int* d, const int N, const int 
 int main(void) {
     srand(time(NULL));
 
-    int* a, * b, * c, * d, * cpu_output;
-    int* dev_a, * dev_b, * dev_c, * dev_d;
+    int* h_a_InputArray, * h_b_InputArray, * h_c_AddMatrix, * h_d_GpuResultMatrix, * d_CpuResultMatrix;
+    int* dev_a_InputArray, * dev_b_InputArray, * dev_c_AddMatrix, * dev_d_GpuResultMatrix;
     cudaEvent_t start, end;
     float time = 0.0;
 
-    int N = 4;
-    int M = 3;
+    int MatrixRows = 512;
+    int MatrixColums = 512;
 
-    a = (int*)malloc(sizeof(int) * N);
-    b = (int*)malloc(sizeof(int) * M);
-    c = (int*)malloc(sizeof(int) * N * M);
-    d = (int*)malloc(sizeof(int) * N * M);
-    cpu_output = (int*)malloc(sizeof(int) * N * M);
+    h_a_InputArray = (int*)malloc(sizeof(int) * MatrixRows);
+    h_b_InputArray = (int*)malloc(sizeof(int) * MatrixColums);
+    h_c_AddMatrix = (int*)malloc(sizeof(int) * MatrixRows * MatrixColums);
+    h_d_GpuResultMatrix = (int*)malloc(sizeof(int) * MatrixRows * MatrixColums);
+    d_CpuResultMatrix = (int*)malloc(sizeof(int) * MatrixRows * MatrixColums);
 
-    init_array(a, N);
-    init_array(b, M);
-    init_mat(c, N, M);
+    init_array(h_a_InputArray, MatrixRows);
+    init_array(h_b_InputArray, MatrixColums);
+    init_mat(h_c_AddMatrix, MatrixRows, MatrixColums);
 
 #ifdef DEBUG
     printf("<<<<<<<<<< initial data:\n");
-    print_array(a, N, "a-vector");
-    print_array(b, M, "b-vector");
-    print_mat(c, N, M, "c-matrix");
+    print_array(h_a_InputArray, MatrixRows, "a-vector");
+    print_array(h_b_InputArray, MatrixColums, "b-vector");
+    print_mat(h_c_AddMatrix, MatrixRows, MatrixColums, "c-matrix");
 #endif // !DEBUG
 
 
-    cudaMalloc((void**)&dev_a, sizeof(int) * N);
-    cudaMalloc((void**)&dev_b, sizeof(int) * M);
-    cudaMalloc((void**)&dev_c, sizeof(int) * N * M);
-    cudaMalloc((void**)&dev_d, sizeof(int) * N * M);
+    cudaMalloc((void**)&dev_a_InputArray, sizeof(int) * MatrixRows);
+    cudaMalloc((void**)&dev_b_InputArray, sizeof(int) * MatrixColums);
+    cudaMalloc((void**)&dev_c_AddMatrix, sizeof(int) * MatrixRows * MatrixColums);
+    cudaMalloc((void**)&dev_d_GpuResultMatrix, sizeof(int) * MatrixRows * MatrixColums);
 
-    cudaMemcpy(dev_a, a, sizeof(int) * N, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, b, sizeof(int) * M, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_c, c, sizeof(int) * N * M, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_a_InputArray, h_a_InputArray, sizeof(int) * MatrixRows, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b_InputArray, h_b_InputArray, sizeof(int) * MatrixColums, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_c_AddMatrix, h_c_AddMatrix, sizeof(int) * MatrixRows * MatrixColums, cudaMemcpyHostToDevice);
 
     cudaEventCreate(&start);
     cudaEventCreate(&end);
@@ -84,88 +84,88 @@ int main(void) {
 #endif // DEBUG
 
     cudaEventRecord(start);
-    kernel << <N * M / 256 + 1, 256 >> > (dev_a, dev_b, dev_c, dev_d, N, M);
+    kernel << <MatrixRows * MatrixColums / 256 + 1, 256 >> > (dev_a_InputArray, dev_b_InputArray, dev_c_AddMatrix, dev_d_GpuResultMatrix, MatrixRows, MatrixColums);
     cudaEventRecord(end);
     cudaEventSynchronize(end);
     cudaEventElapsedTime(&time, start, end);
 
-    cudaMemcpy(d, dev_d, sizeof(int) * N * M, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_d_GpuResultMatrix, dev_d_GpuResultMatrix, sizeof(int) * MatrixRows * MatrixColums, cudaMemcpyDeviceToHost);
 
     printf("Gpu Return with Error Code: %s\n", cudaGetErrorString(cudaGetLastError()));
     printf("\tGPU Time Elapsed: %f ms\n", time);
 
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    cudaFree(dev_c);
-    cudaFree(dev_d);
+    cudaFree(dev_a_InputArray);
+    cudaFree(dev_b_InputArray);
+    cudaFree(dev_c_AddMatrix);
+    cudaFree(dev_d_GpuResultMatrix);
 
-    cpu_calculate_array(a, b, c, cpu_output, N, M);
+    cpu_calculate_array(h_a_InputArray, h_b_InputArray, h_c_AddMatrix, d_CpuResultMatrix, MatrixRows, MatrixColums);
 
 #ifdef DEBUG
     printf(">>>>>>>>>> Gpu final data:\n");
-    print_mat(d, N, M, "d-matrix");
+    print_mat(h_d_GpuResultMatrix, MatrixRows, MatrixColums, "d-matrix");
 
     printf(">>>>>>>>>> Cpu final data:\n");
-    print_mat(cpu_output, N, M, "cpu-matrix");
+    print_mat(d_CpuResultMatrix, MatrixRows, MatrixColums, "cpu-matrix");
 #endif // DEBUG
     auto cpu_start = std::chrono::high_resolution_clock::now();
-    int error = calculate_error(d, cpu_output, N, M);
+    int error = calculate_error(h_d_GpuResultMatrix, d_CpuResultMatrix, MatrixRows, MatrixColums);
     auto cpu_done = std::chrono::high_resolution_clock::now();
     printf("\tCPU Time Elapsed: %f ms\n", std::chrono::duration_cast<std::chrono::microseconds>(cpu_done - cpu_start).count() / 1000.0);
 
     printf("Deviation between Cpu and Gpu: %d\n",error);
 
-    free(a);
-    free(b);
-    free(c);
-    free(d);
-    free(cpu_output);
+    free(h_a_InputArray);
+    free(h_b_InputArray);
+    free(h_c_AddMatrix);
+    free(h_d_GpuResultMatrix);
+    free(d_CpuResultMatrix);
     return 0;
 };
 
-void init_array(int* a, const int N) {
-    int i;
-    for (i = 0; i < N; i++)
+void init_array(int* pArray, const int ArraySize) {
+    int LoopIndex;
+    for (LoopIndex = 0; LoopIndex < ArraySize; LoopIndex++)
     {
-        a[i] = rand() % 4 + 1;
+        pArray[LoopIndex] = rand() % 4 + 1;
     }
 }
-void init_mat(int* a, const int N, const int M) {
-    int i, j;
-    for (i = 0; i < N * M; i++)
+void init_mat(int* pMatrix, const int MatrixRows, const int MatrixColums) {
+    int LoopIndex;
+    for (LoopIndex = 0; LoopIndex < MatrixRows * MatrixColums; LoopIndex++)
     {
-        a[i] = rand() % 4 + 1;
+        pMatrix[LoopIndex] = rand() % 4 + 1;
     }
 }
-void print_array(int* a, const int N, char* d) {
-    int i;
-    for (i = 0; i < N; i++)
-        printf("\n%s[%d]: %d", d, i, a[i]);
+void print_array(int* pArray, const int ArraySize, char* ArrayName) {
+    int LoopIndex;
+    for (LoopIndex = 0; LoopIndex < ArraySize; LoopIndex++)
+        printf("\n%s[%d]: %d", ArrayName, LoopIndex, pArray[LoopIndex]);
     printf("\n");
 }
-void print_mat(int* a, const int N, const int M, char* d) {
-    int i;
-    for (i = 0; i < N*M; i++) {
-        if (i % M == 0)
+void print_mat(int* pMatrix, const int MatrixRows, const int MatrixColums, char* MatrixName) {
+    int LoopIndex;
+    for (LoopIndex = 0; LoopIndex < MatrixRows*MatrixColums; LoopIndex++) {
+        if (LoopIndex % MatrixColums == 0)
         {
-            printf("\n%s[%d]:", d, i);
+            printf("\n%s[%d]:", MatrixName, LoopIndex);
         }
-        printf("\t%d", a[i]);
+        printf("\t%d", pMatrix[LoopIndex]);
     }
     printf("\n");
 }
 
 
-int calculate_error(int* a, int* b, const int N, const int M)
+int calculate_error(int* p_a_Matrix, int* p_b_Matrix, const int MatrixRows, const int MatrixColums)
 {
-    int i, error_result = 0;
-    for (i = 0; i < N * M; i++)
+    int LoopIndex, error_result = 0;
+    for (LoopIndex = 0; LoopIndex < MatrixRows * MatrixColums; LoopIndex++)
     {
-        if (abs(a[i] - b[i]) > 0)
+        if (abs(p_a_Matrix[LoopIndex] - p_b_Matrix[LoopIndex]) > 0)
         {
-            printf("\tIndex %d has error InputA=%d , InputB=%d\n", i, a[i], b[i]);
+            printf("\tIndex %d has error InputA=%d , InputB=%d\n", LoopIndex, p_a_Matrix[LoopIndex], p_b_Matrix[LoopIndex]);
         }
-        error_result += abs(a[i] - b[i]);
+        error_result += abs(p_a_Matrix[LoopIndex] - p_b_Matrix[LoopIndex]);
     }
     return error_result;
 }
